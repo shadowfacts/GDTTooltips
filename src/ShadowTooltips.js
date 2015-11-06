251d71cf482d643438a4054589b99cd169aeaea1
Shadowfacts <me@shadowfacts.net>
2015-11-06 14:05:31 -0500

    Add game report, contract work, and normal work

--- src/ShadowTooltips.js
+++ src/ShadowTooltips.js
@@ -9,26 +9,39 @@ var ShadowTooltips = {};
 			var character = ShadowGDT.getCharacterUnderCursor();
 			if (character != null) {
 				var state = character.state;
+				
 				if (state === CharacterState.Researching) {
 
 					var id = character.currentResearch.id;
 					if (Training.isTraining(id)) {
 
 						var name = Training[id].name;
-						VisualsManager.getCharacterOverlay(character).sayStatic("Training: " + name, 0, 0);
+						if (name === "postMortem") {
+							VisualsManager.getCharacterOverlay(character).sayStatic("Generating Game Report", 34, -75);
+						} else {
+							VisualsManager.getCharacterOverlay(character).sayStatic("Training: " + name, 34, -75);
+						}
 
 					} else {
 
 						var name = Research.getName(character.currentResearch.id);
 						if (typeof name === "undefined") name = character.currentResearch.id;
 						var tooltip = "Researching: " + name;
-						VisualsManager.getCharacterOverlay(character).sayStatic(tooltip, 5, -65);
+						VisualsManager.getCharacterOverlay(character).sayStatic(tooltip, 4, -65);
 
 					}
 
 				} else if (state === CharacterState.Vacation) {
 					
-					VisualsManager.getCharacterOverlay(character).sayStatic("On Vacation", -25, -35);
+					VisualsManager.getCharacterOverlay(character).sayStatic("On Vacation", -27, -35);
+
+				} else if (state === CharacterState.WorkOnContract) {
+
+					VisualsManager.getCharacterOverlay(character).sayStatic("Contract Work", -27, -35);
+
+				} else if (state === CharacterState.Working) {
+
+					VisualsManager.getCharacterOverlay(character).sayStatic("Working", -27, -35);
 
 				}
 			}
